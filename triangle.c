6fd5d475f8ff23c5a9d6c10cf27fa08d0aeeec81
jo <jo@jo-linux.home>
2014-11-27 22:14:20 +0100

    [Simplex]  * Implémentation de la fonction de calcul de la distance verticale d'un sommet par rapport à un triangle

--- triangle.c
+++ triangle.c
@@ -1,11 +1,14 @@
 #include "triangle.h"
 
-void triangle_init(triangle* t, vertex* v0, vertex* v1, vertex* v2, triangle* voisin0, triangle* voisin1, triangle* voisin2)
+#include <math.h>
+
+void triangle_init(triangle* t, vertex* v0, vertex* v1, vertex* v2,
+		   triangle* voisin0, triangle* voisin1, triangle* voisin2)
 {
   t[0] = v0;
   t[1] = v1;
   t[2] = v2;
-  
+
   v[0] = voisin0;
   v[1] = voisin1;
   v[2] = voisin2;
@@ -15,14 +18,14 @@ void triangle_init2(triangle* t, vertex* v0, vertex* v1, vertex* v2)
   t[0] = v0;
   t[1] = v1;
   t[2] = v2;
-  
-  
+
   v[0] = NULL;
   v[1] = NULL;
   v[2] = NULL;
 }
 
-triangle* triangle_create(vertex* v0, vertex* v1, vertex* v2, triangle* voisin0, triangle* voisin1, triangle* voisin2)
+triangle* triangle_create(vertex* v0, vertex* v1, vertex* v2,
+			  triangle* voisin0, triangle* voisin1, triangle* voisin2)
 {
   triangle* t = malloc(sizeof(*t));
 
@@ -35,7 +38,6 @@ triangle* triangle_create2(vertex* v0, vertex* v1, vertex* v2)
   return triangle_create(v0, v1, v2, NULL, NULL, NULL);
 }
 
-//Joel, si tu préfère ton propre code, prend le.
 #define ALIGNE 0x11
 #define GAUCHE 0x01
 #define DROITE 0x10
@@ -68,4 +70,32 @@ int dansTriangle2d(const triangle* t, const vertex* p)
 		}
 	}
 	return 0;
-}	
+}
+
+double triangle_vertical_distance(triangle* t, vertex* v)
+{
+  // Normale (non unitaire) au plan contenant t
+  double nX;
+  double nY;
+  double nZ;
+
+  // Altitude de la projection verticale de v sur le plan contenant t
+  double vProjZ;
+
+  nX = ((t->s[1]->Y - t->s[0]->Y) * (t->s[2]->Z - t->s[0]->Z))
+	- ((t->s[1]->Z - t->s[0]->Z) * (t->s[2]->Y - t->s[0]->Y));
+  nY = ((t->s[1]->Z - t->s[0]->Z) * (t->s[2]->X - t->s[0]->X))
+	- ((t->s[1]->X - t->s[0]->X) * (t->s[2]->Z - t->s[0]->Z));
+  nZ = ((t->s[1]->X - t->s[0]->X) * (t->s[2]->Y - t->s[0]->Y))
+	- ((t->s[1]->Y - t->s[0]->Y) * (t->s[2]->X - t->s[0]->X));
+
+  if (!nZ)
+  {
+    // Erreur d'implémentation: Le maillage n'est pas 2,5 D !
+    exit(-1);
+  }
+
+  vProjZ = (((t->s[0]->X - v->X) * nX) + ((t->s[0]->Y - v->Y) * nY) + (t->s[0]->Z * nZ)) / nZ;
+
+  return (fabs(v->Z - vProjZ));
+}
--- triangle.h
+++ triangle.h
@@ -10,12 +10,14 @@ typedef struct _triangle
   struct _vertex* candidats;	//liste des points dans le triangle (x,y) avec VLINK_CANDIDAT.
 } triangle;
 
-void triangle_init(triangle* t, vertex* v0, vertex* v1, vertex* v2, triangle* voisin0, triangle* voisin1, triangle* voisin2);
+void triangle_init(triangle* t, vertex* v0, vertex* v1, vertex* v2,
+		   triangle* voisin0, triangle* voisin1, triangle* voisin2);
 void triangle_init2(triangle* t, vertex* v0, vertex* v1, vertex* v2);
 
-triangle* triangle_create(vertex* v0, vertex* v1, vertex* v2, triangle* voisin0, triangle* voisin1, triangle* voisin2);
+triangle* triangle_create(vertex* v0, vertex* v1, vertex* v2,
+			  triangle* voisin0, triangle* voisin1, triangle* voisin2);
 triangle* triangle_create2(vertex* v0, vertex* v1, vertex* v2);
 
-double plane_vertical_distance(triangle* t, vertex* v);
+double triangle_vertical_distance(triangle* t, vertex* v);
 
 #endif
