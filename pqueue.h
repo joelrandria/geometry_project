f07fed33e9d2ad4fe4ea9d04e6394e291c147b2f
Joël Randria <jo@jo.com>
2014-12-25 20:11:26 +0100

    [Documentation]  + Documentation settings.h  + Documentation pqueue.h

--- pqueue.h
+++ pqueue.h
@@ -1,28 +1,72 @@
+/**
+ * @file   pqueue.h
+ * @author jo <jo@jo-linux>
+ * @date   Thu Dec 25 19:45:30 2014
+ *
+ * @brief File de priorité de triangles basée sur leurs distances respectives à leurs sommets respectifs.
+ */
 #ifndef __PQUEUE_H__
 #define __PQUEUE_H__
 
 #include "triangle.h"
 
-/*! File de priorité de triangles */
+/**
+ * Définit une file de priorité de triangles.
+ * La priorité des triangles est fondée sur leur distance maximum respective à leurs sommets respectifs.
+ * Cette file de priorité est implémentée selon une arborescence binaire.
+ */
 typedef struct _pqueue
 {
+  /// Taille courante de la file de priorité.
   int size;
+  /// Capacité maximum de la file de priorité.
   int capacity;
+  /// Tableau interne encapsulant l'arborescence binaire de triangles.
   triangle** items;
+
 } pqueue;
 
-/*! Allocation / Destruction */
+/**
+ * Construit une nouvelle file de priorité de triangles, de la capacité spécifiée.
+ *
+ * @param capacity Nombre maximum de triangles pouvant être pris en charge dans la file de priorité.
+ *
+ * @return La file de priorité.
+ */
 pqueue* pqueue_create(int capacity);
+/**
+ * Détruit la file de priorité de triangles spécifiée.
+ *
+ * @param q La file de priorité à libérer.
+ */
 void pqueue_delete(pqueue* q);	//free/delete
 
-/*! Affichage */
+/**
+ * Affiche sur la sortie standard les éléments de la file de priorité spécifiée.
+ *
+ * @param q La file de priorité à afficher.
+ */
 void pqueue_print(pqueue* q);
 
-/*! Modifications */
-void pqueue_enqueue(pqueue* q, triangle* t);	//ajouté dans la queue
-triangle* pqueue_dequeue(pqueue* q);			//retire l'élément de priorité maximum et le retourne
+/**
+ * Insère le triangle spécifié dans la file de priorité spécifiée.
+ *
+ * @param q La file de priorité.
+ * @param t Le triangle à insérer dans la file.
+ */
+void pqueue_enqueue(pqueue* q, triangle* t);
+/**
+ * Défile le triangle de priorité maximum de la file spécifiée.
+ *
+ * @param q La file de priorité à défiler.
+ *
+ * @return Le triangle de priorité maximum de la file spécifiée.
+ */
+triangle* pqueue_dequeue(pqueue* q);
 
-/*! Tests */
+/**
+ * Batterie de tests de la structure de file de priorité.
+ */
 void pqueue_tests();
 
 #endif
--- settings.h
+++ settings.h
@@ -3,41 +3,49 @@
  * @author jo <jo@jo-linux>
  * @date   Thu Dec 25 19:41:36 2014
  *
- * @brief  Définition des types et des données relatifs aux paramètres d'exécution de l'application.
+ * @brief  Types et structures de données relatifs aux paramètres d'exécution de l'application.
  */
 #ifndef __SETTINGS_H__
 #define __SETTINGS_H__
 
-/*! Les modes de visualisation du maillage pris en charge */
+/*! Définit les différents modes de visualisation pris en charge par l'application. */
 typedef enum
 {
-  VIEWMODE_3D = 0,		/**< 3D avec navigation clavier/souris */
-  VIEWMODE_2D = 1,		/**< 2D fil de fer, vue de dessus */
-  VIEWMODE_CONSOLE = 2		/**< Mode console */
+  /// Visualisation 3D avec navigation clavier et souris.
+  VIEWMODE_3D = 0,
+  /// Visualisation 2D fil de fer en vue de dessus. Aucune navigation possible.
+  VIEWMODE_2D = 1,
+  /// Mode console: Aucune visualisation graphique, aucune navigation possible.
+  VIEWMODE_CONSOLE = 2
+
 } viewmode;
 
-/*! Les critères d'arrêt d'affinage du maillage pris en charge */
+/*! Définit les différents critères d'arrêt d'affinage du maillage pris en charge par l'application. */
 typedef enum
 {
-  STOPMODE_NONE = 0,		/**< Insertion de tous les sommets dans le maillage */
-  STOPMODE_FACE_COUNT = 1,	/**< Nombre de faces maximum du maillage */
-  STOPMODE_GOF = 2		/**< Goodness Of Fit */
+  /// Arrêt de l'affinage du maillage une fois tous les sommets existants, insérés dans le maillage.
+  STOPMODE_NONE = 0,
+  /// Arrêt de l'affinage du maillage si un nombre arbitraire maximum de faces est atteint.
+  STOPMODE_FACE_COUNT = 1,
+  /// Arrêt de l'affinage du maillage si le maillage satisfait la valeur de 'Goodness Of Fit' spécifiée.
+  STOPMODE_GOF = 2
+
 } stopmode;
 
-/*! Encapsule les paramètres de configuration de l'application */
+/*! Encapsule l'ensemble des paramètres d'exécution de l'application. */
 typedef struct _settings
 {
-  /*! Nombre de sommets total */
+  /// Nombre total de sommets générés
   int vertex_count;
 
-  /*! Mode de visualisation */
+  /// Mode de visualisation courant
   viewmode view_mode;
 
-  /*! Mode d'arrêt de l'affinage du maillage */
+  /// Mode d'arrêt de l'affinage du maillage
   int stop_mode;
-  /*! Nombre de faces maximum du maillage (paramètre du mode d'arrêt STOPMODE_FACE_COUNT) */
+  /// Nombre de faces maximum du maillage (paramètre du mode d'arrêt STOPMODE_FACE_COUNT)
   int max_face_count;
-  /*! Goodness Of Fit (paramètre du mode d'arrêt STOPMODE_GOF) */
+  /// Goodness Of Fit (paramètre du mode d'arrêt STOPMODE_GOF)
   float gof;
 
 } settings;
